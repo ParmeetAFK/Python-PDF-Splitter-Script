f95de78d10461e905c6481db6513f7b52e2e7791
raj upadhyay <upadhyayraj007@gmail.com>
2020-08-25 15:01:18 +0530

    initial commit

---
+++ pdfsplits_1.py
@@ -0,0 +1,32 @@
+import sys
+from PyPDF2 import PdfFileWriter,PdfFileReader
+
+def pdf_splitter(file,start,end):
+    new_file_name = file.split(".")[0] + " " + "splitted.pdf"
+    read_file = PdfFileReader(open(file,"rb"))
+    new_pdf = PdfFileWriter()
+    start-=1
+    try:
+        with open(new_file_name,"wb") as f:
+            for i in range(start, end):
+              new_pdf.addPage(read_file.getPage(i))
+              new_pdf.write(f)
+              i+=1
+            print("PDF splitted Successfully")
+    except Exception as e:
+        print(e)
+
+if len(sys.argv) < 4:
+    print("*"*50)
+    print("Invalid Agruments")
+    print("-"*50)
+    print("python pdfsplits.py pdf_file_name_with_full_path start_page_number end_page_number")
+    print("-"*50)
+    print("Example")
+    print("python pdfsplits.py 'c:\\\\Users\\\\a.pdf' 2 5")
+    print("*"*50)
+else:
+    file_path = sys.argv[1]
+    start_page = int(sys.argv[2])
+    end_page = int(sys.argv[3])
+    pdf_splitter(file_path,start_page,end_page)
