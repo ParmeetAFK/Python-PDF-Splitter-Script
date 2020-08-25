0c6e4f7c04ad262f3e40b25c4106b1f151f56bf4
raj upadhyay <upadhyayraj007@gmail.com>
2020-08-25 19:19:44 +0530

    added more comments

--- pdfsplits_1.py
+++ pdfsplits_1.py
@@ -2,9 +2,13 @@ import sys
 from PyPDF2 import PdfFileWriter,PdfFileReader
 
 def pdf_splitter(file,start,end):
+    #we will save new splited pdf as "nameofpdf splitted.pdf"
+    #example if pdf name is "abc.pdf" then it will be saved as "abc splitted.pdf"
     new_file_name = file.split(".")[0] + " " + "splitted.pdf"
-    read_file = PdfFileReader(open(file,"rb"))
-    new_pdf = PdfFileWriter()
+
+    read_file = PdfFileReader(open(file,"rb")) #read pdf
+    
+    new_pdf = PdfFileWriter() #create write object
     start-=1
     try:
         with open(new_file_name,"wb") as f:
@@ -18,6 +22,7 @@ def pdf_splitter(file,start,end):
 
 #First step is to check all command line arguments
 if len(sys.argv) < 4:
+    #if arguments are less then 4 then we will show error message to users.
     print("*"*50)
     print("Invalid Agruments")
     print("-"*50)
@@ -27,7 +32,7 @@ if len(sys.argv) < 4:
     print("python pdfsplits.py 'c:\\\\Users\\\\a.pdf' 2 5")
     print("*"*50)
 else:
-    file_path = sys.argv[1]
-    start_page = int(sys.argv[2])
-    end_page = int(sys.argv[3])
+    file_path = sys.argv[1] #file name of PDF file which user want to split
+    start_page = int(sys.argv[2]) #start page number 
+    end_page = int(sys.argv[3]) #end page number
     pdf_splitter(file_path,start_page,end_page)
